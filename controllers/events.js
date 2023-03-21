1d14570c49c74a2aff5f5a0032f6bca5b8fd475d
fgorordo <flgorordo@gmail.com>
2023-03-20 21:40:23 -0300

    finished api

--- controllers/events.js
+++ controllers/events.js
@@ -2,42 +2,108 @@ const { response } = require("express");
 const Event = require("../models/Event");
 
 const getAllEvents = async (req, res = response) => {
+  try {
+    const events = await Event.find().populate("user", "name");
+
     return res.status(200).json({
-        ok: true,
-        msg: "Getting all events",
+      ok: true,
+      events,
     });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Por favor pongase en contacto con el administrador.",
+    });
+  }
 };
 
 const editEvent = async (req, res = response) => {
-    return res.status(200).json({
-        ok: true,
-        msg: "Editing event",
+  const eventId = req.params.id;
+  const _id = req._id;
+  try {
+    const event = await Event.findById(eventId);
+
+    if (!event)
+      return res.status(404).json({
+        ok: false,
+        msg: "No se encontro ningun evento con ese id",
+      });
+
+    if (event.user.toString() !== _id)
+      return res.status(401).json({
+        ok: false,
+        msg: "No puedes editar eventos que fueron creados por otra persona.",
+      });
+
+    const newEvent = {
+      ...req.body,
+      user: _id,
+    };
+
+    const updatedEvent = await Event.findByIdAndUpdate(eventId, newEvent, {
+      new: true,
     });
+    return res.status(200).json({
+      ok: true,
+      updatedEvent,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Por favor pongase en contacto con el administrador.",
+    });
+  }
 };
 
 const removeEvent = async (req, res = response) => {
+  const eventId = req.params.id;
+  const _id = req._id;
+  try {
+    const event = await Event.findById(eventId);
+
+    if (!event)
+      return res.status(404).json({
+        ok: false,
+        msg: "No se encontro ningun evento con ese id",
+      });
+
+    if (event.user.toString() !== _id)
+      return res.status(401).json({
+        ok: false,
+        msg: "No puedes eliminar eventos que fueron creados por otra persona.",
+      });
+
+    const deletedEvent = await Event.findByIdAndDelete(eventId);
     return res.status(200).json({
-        ok: true,
-        msg: "Removing events",
+      ok: true,
     });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Por favor pongase en contacto con el administrador.",
+    });
+  }
 };
 
 const createEvent = async (req, res = response) => {
-    const event = new Event(req.body);
-    try {
-        event.user = req._id;
-        const dbRes = await event.save();
-        return res.json({
-            ok: true,
-            event: dbRes,
-        });
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({
-            ok: false,
-            msg: "Comuniquese con el administrador",
-        });
-    }
+  const event = new Event(req.body);
+  try {
+    event.user = req._id;
+    const dbRes = await event.save();
+    return res.json({
+      ok: true,
+      event: dbRes,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      ok: false,
+      msg: "Comuniquese con el administrador",
+    });
+  }
 };
 
 module.exports = { getAllEvents, editEvent, removeEvent, createEvent };
--- routes/events.js
+++ routes/events.js
@@ -5,10 +5,10 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 const {
-    getAllEvents,
-    editEvent,
-    removeEvent,
-    createEvent,
+  getAllEvents,
+  editEvent,
+  removeEvent,
+  createEvent,
 } = require("../controllers/events");
 const { isDate } = require("../helpers/isDate");
 const { fieldsValidator } = require("../middlewares/fields-validator");
@@ -22,24 +22,35 @@ router.use(tokenValidator);
 
 router.get("/", getAllEvents);
 router.post(
-    "/",
-    [
-        check("title", "Debes ingresar un titulo para el evento")
-            .not()
-            .isEmpty(),
-        check("notes", "Debes ingresar un texto").isString(),
-        check(
-            "start",
-            "Debes ingresar una fecha de inicio para el evento"
-        ).custom(isDate),
-        check("end", "Debes ingresar una fecha de fin para el evento").custom(
-            isDate
-        ),
-        fieldsValidator,
-    ],
-    createEvent
+  "/",
+  [
+    check("title", "Debes ingresar un titulo para el evento").not().isEmpty(),
+    check("notes", "Debes ingresar un texto").isString(),
+    check("start", "Debes ingresar una fecha de inicio para el evento").custom(
+      isDate
+    ),
+    check("end", "Debes ingresar una fecha de fin para el evento").custom(
+      isDate
+    ),
+    fieldsValidator,
+  ],
+  createEvent
 );
-router.put("/:id", editEvent);
+router.put(
+  "/:id",
+  [
+    check("title", "Debes ingresar un titulo para el evento").not().isEmpty(),
+    check("notes", "Debes ingresar un texto").isString(),
+    check("start", "Debes ingresar una fecha de inicio para el evento").custom(
+      isDate
+    ),
+    check("end", "Debes ingresar una fecha de fin para el evento").custom(
+      isDate
+    ),
+    fieldsValidator,
+  ],
+  editEvent
+);
 router.delete("/:id", removeEvent);
 
 module.exports = router;
